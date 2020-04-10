acda12d6ea38e6fc57b67133c3876ea717a4f8fb
DarkManPC <guillaume.longrais@orange.fr>
2020-04-10 15:32:33 +0200

    computing without memory optimisation

--- life_kernel.cu
+++ life_kernel.cu
@@ -22,9 +22,36 @@ __global__ void life_kernel(int * source_domain, int * dest_domain,
 	                       domain_x, domain_y);
     
     // TODO: Read the 8 neighbors and count number of blue and red
-
-	// TODO: Compute new value
+    int blue = 0, red = 0;
+    for(int i = -1; i < 2; i++){
+        for(int j = -1; j < 2; j++){
+            if(i != 0 && j !=0){
+                int cell = read_cell(source_domain, tx, ty, i, j, domain_x, domain_y);
+                if(cell == 1){
+                    red++;
+                }else if(cell == 2){
+                    blue++;
+                }
+            }
+        }
+    }
+    // TODO: Compute new value
+    
+    if(myself == 0 && (blue + red) == 3){
+        if(red >= 2){
+            myself = 1; 
+        }else{
+            myself = 2;
+        }
+    }else if((blue + red) < 2 || (blue + red) > 3){
+        myself = 0;
+    }
 	
-	// TODO: Write it in dest_domain
+    // TODO: Write it in dest_domain
+    
+    int x = (unsigned int)tx % domain_x;
+    int y = (unsigned int)ty % domain_y;
+
+    dest_domain[y * domain_x + x];
 }
 
