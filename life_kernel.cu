0fd47177b07715fbdad442d75a8c04552eb29cc0
DarkManPC <guillaume.longrais@orange.fr>
2020-04-21 22:56:28 +0200

    fixing

--- life_kernel.cu
+++ life_kernel.cu
@@ -17,7 +17,7 @@ __global__ void life_kernel(int * source_domain, int * dest_domain,
     
     if(tx >= domain_x || ty >= domain_y) return;
 
-    __shared__ int data_block[];
+    extern __shared__ int data_block[];
 
     data_block[(threadIdx.y+1) * blockDim.x + (threadIdx.x+1)] = read_cell(source_domain, tx, ty, 0, 0,
         domain_x, domain_y);
@@ -54,6 +54,8 @@ __global__ void life_kernel(int * source_domain, int * dest_domain,
         data_block[(threadIdx.y+2) * blockDim.x + (threadIdx.x+2)] = read_cell(source_domain, tx, ty, +1, +1,
             domain_x, domain_y);
     }
+
+    __syncthread();
 
 
     // Read cell
